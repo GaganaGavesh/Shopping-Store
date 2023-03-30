c96f09c0535e5dd7a191d0b5a2853bd1691435fb
Gagana Gavesh <gaveshgamage@gmail.com>
2023-03-30 22:02:55 +0530

    Removed unnecessary imports

--- src/containers/layout/MainNavigation.js
+++ src/containers/layout/MainNavigation.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import classes from './MainNavigation.module.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
-    return ( 
-        <header className={classes.header}>
-            <div className={classes.logo}>Thili</div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/productItems">Women</Link>
-                        </li>
-                        <li>
-                            <Link to="/productItems">Men</Link>
-                        </li>
-                        <li>
-                            <Link to="/productItems">Kids</Link>
-                        </li>
-                    </ul>
-                </nav>
-        </header>
-     );
-}
- 
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>Thili</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/productItems">Women</Link>
+          </li>
+          <li>
+            <Link to="/">Men</Link>
+          </li>
+          <li>
+            <Link to="/productItems">Kids</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
 export default MainNavigation;
