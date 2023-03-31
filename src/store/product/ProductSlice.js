6da20e5f8e53aa64d80f084b8100fa9b084e32a6
Gagana Gavesh <gaveshgamage@gmail.com>
2023-04-01 00:26:48 +0530

    Added dummy product data and configured with redux store

--- src/store/product/ProductSlice.js
+++ src/store/product/ProductSlice.js
@@ -1,29 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+import PRODUCTS from "../../data/DummyData";
+
 export const productSlice = createSlice({
   name: "product",
   initialState: {
-    value: 0,
-  },
-  reducers: {
-    increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1;
-    },
-    decrement: (state) => {
-      state.value -= 1;
-    },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload;
-    },
+    availableProducts: PRODUCTS,
+    userProducts: PRODUCTS.filter((prod) => prod.ownerId === "u1"),
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = productSlice.actions;
+//Actions are commented since there ane no any.
+// export const { } = productSlice.actions;
 
 export default productSlice.reducer;
 
